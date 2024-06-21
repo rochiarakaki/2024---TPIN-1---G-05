a8b6b9f88beca5d89078d646015e69c6ed12ae6c
octaviopenaa <openaiannello@pioix.edu.ar>
2024-06-21 15:08:25 -0300

    Pestaña del tablero de juego(Octavio Pena)

---
+++ Backend/index.js
@@ -0,0 +1,40 @@
+var express = require('express'); //Tipo de servidor: Express
+var bodyParser = require('body-parser'); //Convierte los JSON
+var cors = require('cors');
+
+var app = express(); //Inicializo express
+var port = process.env.PORT || 3000; //Ejecuto el servidor en el puerto 3000
+
+// Convierte una petición recibida (POST-GET...) a objeto JSON
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', function(req, res){
+    res.status(200).send({
+        message: 'GET Home route working fine!'
+    });
+});
+
+/**
+ * req = request. en este objeto voy a tener todo lo que reciba del cliente
+ * res = response. Voy a responderle al cliente
+ */
+app.get('/saludo', function(req,res){
+    console.log(req.query) //Los pedidos get reciben los datos del req.query
+    res.send({respuesta: `Respuesta del Backend`})
+})
+
+app.post('/nombreDelPedido', function(req,res) {
+    console.log(req.body) //Los pedidos post reciben los datos del req.body
+    res.send("ok")
+})
+
+//Pongo el servidor a escuchar
+app.listen(port, function(){
+    console.log(`Server running in http://localhost:${port}`);
+    console.log('Defined routes:');
+    console.log('   [GET] http://localhost:3000/');
+    console.log('   [GET] http://localhost:3000/saludo');
+    console.log('   [POST] http://localhost:3000/nombreDelPedido');
+});
